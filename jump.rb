6393a1e0d25502c446a853b84651c7f40053be13
Robby Ranshous <rranshous@gmail.com>
2018-01-02 16:42:24 -0500

    actor

--- jump.rb
+++ jump.rb
@@ -1,12 +1,57 @@
 require_relative 'lib'
 
+class Actor
+
+  CHROMOSOMES = {
+    MORE_JUMP: PatternChromosome.new(pattern:  [0, 0, 0, 0, 1, 1, 1, 0], affect: [0,  1]),
+    MORE_LEFT: PatternChromosome.new(pattern:  [0, 0, 0, 1, 0, 1, 1, 1], affect: [-1, 0]),
+    MORE_DUCK: PatternChromosome.new(pattern:  [1, 0, 1, 1, 1, 1, 0, 0], affect: [0, -1]),
+    MORE_RIGHT: PatternChromosome.new(pattern: [1, 1, 0, 1, 1, 0, 1, 1], affect: [1,  0]),
+    LESS_JUMP: PatternChromosome.new(pattern:  [1, 0, 0, 1, 1, 0, 1, 0], affect: [0, -1]),
+    LESS_LEFT: PatternChromosome.new(pattern:  [1, 1, 0, 0, 1, 1, 1, 1], affect: [1,  0]),
+    LESS_DUCK: PatternChromosome.new(pattern:  [1, 1, 0, 0, 0, 1, 0, 1], affect: [0,  1]),
+    LESS_RIGHT: PatternChromosome.new(pattern: [1, 1, 0, 1, 0, 1, 1, 1], affect: [-1, 0]),
+  }
+
+  def actions genome
+    moves = expressions(genome)
+              .group_by { |e| e.first }
+              .to_a
+              .sort_by { |(_, es)| es.length }
+              .map { |(e, _)| e }
+    moves
+  end
+
+  def chromosomes
+    CHROMOSOMES.values
+  end
+
+  def expressions genome
+    chromosomes.reduce([]) do |expressions, chromosome|
+      expressions += chromosome.alleles genome
+    end
+  end
+end
 
 class PatternChromosome
-  attr_accessor :pattern, :count
+  attr_accessor :pattern, :count, :affect
 
-  def initialize pattern, count=1
+  def initialize pattern: [], count: 1, affect: []
     self.pattern = pattern
-    self.count = count
+    self.count   = count
+    self.affect  = affect
+  end
+
+  def alleles genome
+    [affect] * activation_level
+  end
+
+  def active? genome
+    activation_level > 0
+  end
+
+  def activation_level genome
+    1 + find_occurances(genome).length - count
   end
 
   def distance genome
@@ -39,12 +84,6 @@ end
 
 if __FILE__ == $0
   require 'pry'
-
-  patterns = [
-    [1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0,
-     1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0]
-  ]
-  patterns = [[ 1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,1,0 ]]
   puts "running sim"
   chromosomes = patterns.map { |p| PatternChromosome.new(p) }
   sim = Sim.new(ChromosomeFitnessChecker.new(*chromosomes))
