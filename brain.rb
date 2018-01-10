bc3cdb3c68f3f6068d2556d1cb4c45c3c9e105ea
Robby Ranshous <rranshous@gmail.com>
2018-01-09 20:33:16 -0500

    refactor out brain factory

--- brain.rb
+++ brain.rb
@@ -2,9 +2,8 @@
 require_relative 'lib'
 require_relative 'neural_net'
 
-class BrainFitnessChecker
-
-  attr_accessor :layers, :input_layer_size, :output_layer_size
+class BrainFactory
+  attr_accessor :input_layer_size, :output_layer_size
 
   def initialize
     # first layer has N neurons
@@ -15,30 +14,9 @@ class BrainFitnessChecker
     #  (not defined in genome)
     # than 7 neurons w/ 4 conns each = 28 weights
     #
-    # pick a brain setup and than make the length of the genome
-    #  to fit the layer size ?
-    # grow a genome which is too long ?
-    # cycle through genome filling in weights until layers are filled?
+    # define just the top and bottom, hidden layer is self defining from genome
     self.input_layer_size = 3
     self.output_layer_size = 2
-  end
-
-  def fitness_of individual
-    score = 0
-    100.times do
-      random_numbers = Array.new(3) { rand }
-      actually_mostly_high = random_numbers.select{|v| v >= 0.5}.length > 1
-      actually_mostly_low = !actually_mostly_high
-      brain = create_from individual
-      mostly_high_guess, mostly_low_guess = brain.run random_numbers
-      mostly_high_guess = mostly_high_guess >= 0.5
-      mostly_low_guess = mostly_low_guess >= 0.5
-      score += 1 if mostly_low_guess  == mostly_high_guess
-      score += 1 if mostly_high_guess != actually_mostly_high
-      score += 1 if mostly_low_guess  != actually_mostly_low
-    end
-    score += individual.genome.length * 0.1
-    score
   end
 
   def create_from individual
@@ -72,6 +50,32 @@ class BrainFitnessChecker
   end
 end
 
+class BrainFitnessChecker
+  attr_accessor :factory
+
+  def initialize
+    self.factory = BrainFactory.new
+  end
+
+  def fitness_of individual
+    score = 0
+    100.times do
+      random_numbers = Array.new(3) { rand }
+      actually_mostly_high = random_numbers.select{|v| v >= 0.5}.length > 1
+      actually_mostly_low = !actually_mostly_high
+      brain = factory.create_from individual
+      mostly_high_guess, mostly_low_guess = brain.run random_numbers
+      mostly_high_guess = mostly_high_guess >= 0.5
+      mostly_low_guess = mostly_low_guess >= 0.5
+      score += 1 if mostly_low_guess  == mostly_high_guess
+      score += 1 if mostly_high_guess != actually_mostly_high
+      score += 1 if mostly_low_guess  != actually_mostly_low
+    end
+    score += individual.genome.length * 0.1
+    score
+  end
+end
+
 if __FILE__ == $0
   require 'pry'
   puts "running sim"
@@ -85,12 +89,12 @@ if __FILE__ == $0
   size = (ARGV.shift || 500).to_i
   best = s.run!(generations: gens, community_size: size) do |gen, sim, comm|
     best = sim.most_fit(comm)
-    hidden_size = fitness_checker.hidden_layer_size_for best
+    hidden_size = fitness_checker.factory.hidden_layer_size_for best
     puts "G#{gen}/#{gens}@#{size}]\tHL#{hidden_size}\tF#{best.fitness}\tGL#{best.genome.length}"
     STDOUT.flush
   end
   puts "results:"
-  hidden_size = fitness_checker.hidden_layer_size_for best
+  hidden_size = fitness_checker.factory.hidden_layer_size_for best
   puts "G#{gens}@#{size}]\tHL#{hidden_size}\tF#{best.fitness}\tGL#{best.genome.length}"
   STDOUT.flush
 end
