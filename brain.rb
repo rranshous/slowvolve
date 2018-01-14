be3ed7534239521a2091d39c9bd72c2812f9275a
Robby Ranshous <rranshous@gmail.com>
2018-01-14 11:36:24 -0500

    refactor

--- brain.rb
+++ brain.rb
@@ -1,54 +1,5 @@
 
 require_relative 'lib'
-require_relative 'neural_net'
-
-class BrainFactory
-  attr_accessor :input_layer_size, :output_layer_size
-
-  def initialize
-    # first layer has N neurons
-    # next layer has M neurons
-    # each of the M neurons will have N + 1 weights
-    # first layer has no weights
-    # below would have 3 neurons w/ no weights
-    #  (not defined in genome)
-    # than 7 neurons w/ 4 conns each = 28 weights
-    #
-    # define just the top and bottom, hidden layer is self defining from genome
-    self.input_layer_size = 3
-    self.output_layer_size = 2
-  end
-
-  def create_from individual
-    genome = individual.genome.dup
-    weights = {}
-    hidden_layer_size = hidden_layer_size_for individual
-    if hidden_layer_size == 0
-      layers =  [input_layer_size, output_layer_size]
-    else
-      layers =  [input_layer_size, hidden_layer_size, output_layer_size]
-    end
-    pointer = 0
-    layers[1..-1].each_with_index do |layer_size, i|
-      weights[i+1] = []
-      (layer_size+1).times do
-        weights[i+1] << Array.new(layers[i]) {
-          weight = genome[pointer]
-          pointer += 1
-          pointer = pointer % genome.length
-          weight
-        }
-      end
-    end
-    nn = NeuralNet.new layers
-    nn.weights = weights
-    nn
-  end
-
-  def hidden_layer_size_for individual
-    individual.genome.reduce(:+).round.abs
-  end
-end
 
 class BrainFitnessChecker
   attr_accessor :factory
