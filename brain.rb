30aed379a0b0fb03dc2dda9e7bcdc1da15ca7933
Robby Ranshous <rranshous@gmail.com>
2018-05-19 23:52:34 -0400

    refactor, working on color rec

--- brain.rb
+++ brain.rb
@@ -1,37 +1,60 @@
 
 require_relative 'lib'
+require_relative 'brain/lib'
+require 'json'
 
-class BrainFitnessChecker
-  attr_accessor :factory
+module Brain::FitnessChecker
+  class Blah
+    # turn on when most the input fits a pattern
+    # is the image mostly red ?
+    attr_accessor :factory
 
-  def initialize
-    self.factory = BrainFactory.new
-  end
-
-  def fitness_of individual
-    score = 0
-    100.times do
-      random_numbers = Array.new(3) { rand }
-      actually_mostly_high = random_numbers.select{|v| v >= 0.5}.length > 1
-      actually_mostly_low = !actually_mostly_high
-      brain = factory.create_from individual
-      mostly_high_guess, mostly_low_guess = brain.run random_numbers
-      mostly_high_guess = mostly_high_guess >= 0.5
-      mostly_low_guess = mostly_low_guess >= 0.5
-      score += 1 if mostly_low_guess  == mostly_high_guess
-      score += 1 if mostly_high_guess != actually_mostly_high
-      score += 1 if mostly_low_guess  != actually_mostly_low
+    def initialize
+      self.factory = BrainFactory.new input_layer_size: 300,
+                                      output_layer_size: 2
     end
-    score += individual.genome.length * 0.1
-    score
+
+    def fitness_of individual
+      score = 0
+      100.times do
+        brain = factory.create_from individual
+        image_details = generate_random_image
+        mostly_red, mostly_not_red = brain.run image_details.data
+        mostly_red = mostly_red >= 0.5
+        mostly_not_red = mostly_not_red >= 0.5
+        score += 1 if mostly_red != image_details.is_red
+        score += 1 if mostly_not_red == image_details.is_red
+        score += 2 if mostly_red == mostly_not_red
+      end
+      score += individual.genome.length * 0.1
+      score
+    end
+
+    def generate_random_image
+      # 10 by 10 pixel color image
+      red_pixels = 0
+      image_data = []
+      10.times do |i|
+        10.times do |j|
+          3.times do |c|
+            image_data << rand(0..255)
+          end
+          if image_data[-3..-1].max == image_data[-2]
+            red_pixels += 1
+          end
+        end
+      end
+      OpenStruct.new({ data: image_data, is_red: red_pixels >= 50 })
+    end
   end
 end
+
 
 if __FILE__ == $0
   require 'pry'
   puts "running sim"
   puts "VERSION: self defining hidden layer, variable len starter genes, mutate can remove gene, genome length in fitness test"
-  fitness_checker = BrainFitnessChecker.new
+  fitness_checker = Brain::FitnessChecker::HighOrLow.new
   Individual::VariableGeneLength = true
   Individual::GENOME_LENGTH = 200
   puts "base GENOME_LENGTH: #{Individual::GENOME_LENGTH}"
@@ -44,8 +67,11 @@ if __FILE__ == $0
     puts "G#{gen}/#{gens}@#{size}]\tHL#{hidden_size}\tF#{best.fitness}\tGL#{best.genome.length}"
     STDOUT.flush
   end
+  puts
   puts "results:"
   hidden_size = fitness_checker.factory.hidden_layer_size_for best
   puts "G#{gens}@#{size}]\tHL#{hidden_size}\tF#{best.fitness}\tGL#{best.genome.length}"
+  puts
+  puts best.genome.to_json
   STDOUT.flush
 end
---
+++ brain/lib.rb
@@ -0,0 +1,29 @@
+module Brain
+  module FitnessChecker
+    class HighOrLow
+      attr_accessor :factory
+
+      def initialize
+        self.factory = BrainFactory.new
+      end
+
+      def fitness_of individual
+        score = 0
+        100.times do
+          random_numbers = Array.new(3) { rand }
+          actually_mostly_high = random_numbers.select{|v| v >= 0.5}.length > 1
+          actually_mostly_low = !actually_mostly_high
+          brain = factory.create_from individual
+          mostly_high_guess, mostly_low_guess = brain.run random_numbers
+          mostly_high_guess = mostly_high_guess >= 0.5
+          mostly_low_guess = mostly_low_guess >= 0.5
+          score += 1 if mostly_low_guess  == mostly_high_guess
+          score += 1 if mostly_high_guess != actually_mostly_high
+          score += 1 if mostly_low_guess  != actually_mostly_low
+        end
+        score += individual.genome.length * 0.1
+        score
+      end
+    end
+  end
+end
